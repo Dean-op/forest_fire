d0d341b23ff6ff2b212cda0992364dd87b9429c9
Dean-op <62128239+Dean-op@users.noreply.github.com>
2026-04-28 20:26:28 +0800

    fix: change backend main parameter

--- forest_fire_backend/main.py
+++ forest_fire_backend/main.py
@@ -2,8 +2,8 @@ import uvicorn
 
 
 def main() -> None:
-    uvicorn.run('app.main:app', host='0.0.0.0', port=8000, reload=False)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8010, reload=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
